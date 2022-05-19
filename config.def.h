fd5b92b239e089b2a587eaa0498d3de81ac33b89
UserCtf777 <ukantjadia0120@gmail.com>
2022-05-20 02:25:32 +0530

    Daily Upadate

--- config.def.h
+++ config.def.h
@@ -153,13 +153,13 @@ unsigned int tabspaces = 8;
 
 #if ALPHA_PATCH
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 #if ALPHA_GRADIENT_PATCH
-float grad_alpha = 0.54; //alpha value that'll change
-float stat_alpha = 0.46; //constant alpha value that'll get added to grad_alpha
+float grad_alpha = 0.74; //alpha value that'll change
+float stat_alpha = 0.66; //constant alpha value that'll get added to grad_alpha
 #endif // ALPHA_GRADIENT_PATCH
 #if ALPHA_FOCUS_HIGHLIGHT_PATCH
-float alphaUnfocused = 0.6;
+float alphaUnfocused = 0.8;
 #endif // ALPHA_FOCUS_HIGHLIGHT_PATCH
 #endif // ALPHA_PATCH
 
